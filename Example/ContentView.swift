4fff04427251ca39b6f9bd75a2cdc380223a7c4f
Koji Murata <koji.murata@dena.com>
2019-09-03 01:49:22 +0900

    example target

---
+++ Example/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Koji Murata on 2019/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var 
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
