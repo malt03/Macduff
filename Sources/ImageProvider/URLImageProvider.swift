f7e54409b1c7fe2518ab710cc0548ac2ece9f59f
Koji Murata <koji.murata@dena.com>
2019-09-05 01:45:55 +0900

    fix

--- Sources/ImageProvider/URLImageProvider.swift
+++ Sources/ImageProvider/URLImageProvider.swift
@@ -14,11 +14,6 @@ struct RemoteURLImageProvider: ImageProvider {
     let url: URL
         
     func run(progress: @escaping (Float) -> Void, success: @escaping (ProvidingImage) -> Void, failure: @escaping (Error) -> Void) {
-        for i in 0..<10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + TimeInterval(i) / 5, execute: {
-                progress(Float(i) / 10)
-            })
-        }
-//        ImageDownloader(url: url).download(progress: progress, success: success, failure: failure)
+        ImageDownloader(url: url).download(progress: progress, success: success, failure: failure)
     }
 }
