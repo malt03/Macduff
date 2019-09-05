e1ca6a57f384fa5f67645d0db74e763414bc722a
Koji Murata <koji.murata@dena.com>
2019-09-05 21:13:55 +0900

    watchOS sample

---
+++ Examples/Example-watchOS/Example-watchOS WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Example-watchOS WatchKit Extension
+//
+//  Created by Koji Murata on 2019/09/05.
+//  Copyright © 2019 Koji Murata. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
