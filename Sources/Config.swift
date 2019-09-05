ebb9c340372941be28f1559d1c289651a58633fd
Koji Murata <koji.murata@dena.com>
2019-09-06 01:38:15 +0900

    default config

--- Sources/Config.swift
+++ Sources/Config.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct Config {
-    public static var `default` = Config()
+    public static var `default` = Config(transition: .opacity, fetchTrigger: .appear, cache: MemoryCache(), cacheTTL: 60 * 60 * 24 * 30)
     
     public let transition: AnyTransition
     public let fetchTrigger: FetchTrigger
@@ -16,10 +16,10 @@ public struct Config {
     public let cacheTTL: TimeInterval
     
     public init(
-        transition: AnyTransition = .opacity,
-        fetchTrigger: FetchTrigger = .appear,
-        cache: Cache = MemoryCache(),
-        cacheTTL: TimeInterval = 60 * 60 * 24 * 30
+        transition: AnyTransition = Config.default.transition,
+        fetchTrigger: FetchTrigger = Config.default.fetchTrigger,
+        cache: Cache = Config.default.cache,
+        cacheTTL: TimeInterval = Config.default.cacheTTL
     ) {
         self.transition = transition
         self.fetchTrigger = fetchTrigger
