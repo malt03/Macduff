013c63234f32b4dcc2e20c8e709e7f319d8b7b50
Koji Murata <koji.murata@dena.com>
2019-09-05 23:29:27 +0900

    mac

---
+++ Examples/Example-macOS/Example-macOS/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Example-macOS
+//
+//  Created by Koji Murata on 2019/09/05.
+//  Copyright © 2019 Koji Murata. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+
+        window.contentView = NSHostingView(rootView: ContentView())
+
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Examples/Example-macOS/Example-macOS/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Example-macOS
+//
+//  Created by Koji Murata on 2019/09/03.
+//
+
+import SwiftUI
+import Macduff
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            NavigationLink(destination: ImagesView()) { Text("Show Images") }
+        }
+    }
+}
+
+struct ImagesView: View {
+    @State private var randomImages = (0..<50).map { RandomImageModel(id: $0) }
+    
+    var body: some View {
+        List(randomImages) { (image) in
+            HStack {
+                RemoteImage(source: image.url, loadingPlaceHolder: { (progress) -> ProgressView in
+                    return ProgressView(progress: progress)
+                }, errorPlaceHolder: { _ in
+                    Rectangle().fill(Color.red)
+                })
+                    .scaledToFill()
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipped()
+                    .cornerRadius(4)
+                Text(image.url.absoluteString).font(Font.system(.subheadline))
+            }
+        }
+    }
+}
+
+struct ProgressView: View {
+    let progress: Float
+    
+    var body: some View {
+        return GeometryReader { (geometry) in
+            ZStack(alignment: .bottom) {
+                Rectangle().fill(Color.gray)
+                Rectangle().fill(Color.green)
+                    .frame(width: nil, height: geometry.frame(in: .global).height * CGFloat(self.progress), alignment: .bottom)
+            }
+        }
+        
+    }
+}
+
+struct RandomImageModel: Identifiable {
+    let id: Int
+    var url: URL { return URL(string: "https://picsum.photos/200/100?random=\(id)")! }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
