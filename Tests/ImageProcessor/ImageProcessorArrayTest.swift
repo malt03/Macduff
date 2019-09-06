e29d1f75b432d4d858009deda09443bd898b084d
Koji Murata <koji.murata@dena.com>
2019-09-07 00:41:18 +0900

    arrayimageprocessor test

--- Tests/ImageProcessor/ImageProcessorArrayTest.swift
+++ Tests/ImageProcessor/ImageProcessorArrayTest.swift
@@ -5,4 +5,35 @@
 //  Created by Koji Murata on 2019/09/07.
 //
 
-import Foundation
+import XCTest
+@testable import Macduff
+
+final class ImageProcessorArrayTest: XCTestCase {
+    
+    struct Processor: ImageProcessor {
+        let cacheKey: String
+        let callHandler: () -> Void
+        
+        func process(image: NativeImage) -> NativeImage? {
+            callHandler()
+            return image
+        }
+    }
+    
+    func test() {
+        var step = 0
+        let processor1 = Processor(cacheKey: "p1") {
+            XCTAssertEqual(step, 0)
+            step += 1
+        }
+        let processor2 = Processor(cacheKey: "p2") {
+            XCTAssertEqual(step, 1)
+            step += 1
+        }
+        let arrayProcessor = [processor1, processor2] as ImageProcessor
+        _ = arrayProcessor.process(image: getImage(forResource: "Lena"))
+        
+        XCTAssertEqual(step, 2)
+        XCTAssertEqual(arrayProcessor.cacheKey, "p1>p2")
+    }
+}
